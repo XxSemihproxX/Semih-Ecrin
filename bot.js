919b7f4d97024e73974e592d24515cb4c0952c80
Smeih <50422145+XxSemihproxX@users.noreply.github.com>
2019-07-20 10:15:27 +0300

    Create bot.js

---
+++ bot.js
@@ -0,0 +1,341 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+const ayarlar = require('./ayarlar.json');
+const chalk = require('chalk');
+const fs = require('fs');
+const moment = require('moment');
+require('./util/eventLoader')(client);
+
+var prefix = ayarlar.prefix; "se!" //prefix yazınız...
+
+const log = message => {
+  console.log(`[${moment().format('YYYY-MM-DD HH:mm:ss')}] ${message}`);
+};
+
+client.commands = new Discord.Collection();
+client.aliases = new Discord.Collection();
+fs.readdir('./komutlar/', (err, files) => {
+  if (err) console.error(err);
+  log(`${files.length} komut yüklenecek.`);
+  files.forEach(f => {
+    let props = require(`./komutlar/${f}`);
+    log(`Yüklenen komut: ${props.help.name}.`);
+    client.commands.set(props.help.name, props);
+    props.conf.aliases.forEach(alias => {
+      client.aliases.set(alias, props.help.name);
+    });
+  });
+});
+
+client.reload = command => {
+  return new Promise((resolve, reject) => {
+    try {
+      delete require.cache[require.resolve(`./komutlar/${command}`)];
+      let cmd = require(`./komutlar/${command}`);
+      client.commands.delete(command);
+      client.aliases.forEach((cmd, alias) => {
+        if (cmd === command) client.aliases.delete(alias);
+      });
+      client.commands.set(command, cmd);
+      cmd.conf.aliases.forEach(alias => {
+        client.aliases.set(alias, cmd.help.name);
+      });
+      resolve();
+    } catch (e){
+      reject(e);
+    }
+  });
+};
+
+client.load = command => {
+  return new Promise((resolve, reject) => {
+    try {
+      let cmd = require(`./komutlar/${command}`);
+      client.commands.set(command, cmd);
+      cmd.conf.aliases.forEach(alias => {
+        client.aliases.set(alias, cmd.help.name);
+      });
+      resolve();
+    } catch (e){
+      reject(e);
+    }
+  });
+};
+
+client.unload = command => {
+  return new Promise((resolve, reject) => {
+    try {
+      delete require.cache[require.resolve(`./komutlar/${command}`)];
+      let cmd = require(`./komutlar/${command}`);
+      client.commands.delete(command);
+      client.aliases.forEach((cmd, alias) => {
+        if (cmd === command) client.aliases.delete(alias);
+      });
+      resolve();
+    } catch (e){
+      reject(e);
+    }
+  });
+};
+
+client.on('message', msg => {
+  if (msg.content.toLowerCase() === 'sa') {
+    msg.reply('Aleyküm Selam Knk Hoşgeldin');
+  }
+});
+
+client.elevation = message => {
+  if(!message.guild) {
+	return; }
+  let permlvl = 0;
+  if (message.member.hasPermission("BAN_MEMBERS")) permlvl = 2;
+  if (message.member.hasPermission("ADMINISTRATOR")) permlvl = 3;
+  if (message.author.id === ayarlar.sahip) permlvl = 4;
+  return permlvl;
+};
+
+var regToken = /[\w\d]{24}\.[\w\d]{6}\.[\w\d-_]{27}/g;
+// client.on('debug', e => {
+//   console.log(chalk.bgBlue.green(e.replace(regToken, 'that was redacted')));
+// });
+
+client.on('warn', e => {
+  console.log(chalk.bgYellow(e.replace(regToken, 'that was redacted')));
+});
+
+client.on('error', e => {
+  console.log(chalk.bgRed(e.replace(regToken, 'that was redacted')));
+});
+client.on('message', msg => {
+  const reason = msg.content.split(" ").slice(1).join(" ");
+  if (msg.channel.name== '🔧destek') { 
+    const hatay = new Discord.RichEmbed()
+    .addField(" Hata ", `Bu Sunucuda \`Destek\` Adında Bir Rol Yok!`)
+    .setColor("RANDOM")
+    
+    if (!msg.guild.roles.exists("name", "Destek Ekibi")) return msg.author.send(hatay) + msg.guild.owner.send(`${msg.guild.name} Adlı Sunucunda, \`Destek\` Adlı Bir Rol Olmadığı İçin, Hiçkimse Destek Talebi Açamıyor!`);
+    if(msg.guild.channels.find('name', 'Talepler')) {
+      msg.guild.createChannel(`destek-${msg.author.id}`, "text").then(c => {
+      const category = msg.guild.channels.find('name', 'Talepler')
+      c.setParent(category.id)
+      let role = msg.guild.roles.find("name", "Destek Ekibi");
+      let role2 = msg.guild.roles.find("name", "@everyone");
+      c.overwritePermissions(role, {
+          SEND_MESSAGES: false,
+          READ_MESSAGES: false
+      });
+      c.overwritePermissions(role2, {
+          SEND_MESSAGES: false,
+          READ_MESSAGES: false
+      });
+      c.overwritePermissions(msg.author, {
+          SEND_MESSAGES: true,
+          READ_MESSAGES: true
+      });
+
+      const embed = new Discord.RichEmbed()
+      .setColor("RANDOM")
+      .setAuthor(`${client.user.username} | Destek Sistemi`)
+      .addField(`Merhaba ${msg.author.username}!`, `Destek Yetkilileri burada seninle ilgilenecektir. \nDestek talebini kapatmak için \`${prefix}kapat\` yazabilirsin.`)
+      .addField(`» Talep Konusu/Sebebi:`, `${msg.content}`, true)
+      .addField(`» Kullanıcı:`, `<@${msg.author.id}>`, true)
+      .setFooter(`${client.user.username} | Destek Sistemi`)
+      .setTimestamp()
+      c.send({ embed: embed });
+      c.send(`<@${msg.author.id}> Adlı kullanıcı "\`${msg.content}\`" sebebi ile destek talebi açtı! Lütfen Destek Ekibini bekle, @here`)
+      msg.delete()
+      }).catch(console.error);
+    }
+  }
+});
+  
+client.on("message", message => {
+if (message.content.toLowerCase().startsWith(prefix + `kapat`)) {
+    if (!message.channel.name.startsWith(`destek-`)) return message.channel.send(`Bu komut sadece Destek Talebi kanallarında kullanılablir!`);
+
+    var deneme = new Discord.RichEmbed()
+    .setColor("RANDOM")
+    .setAuthor(`Destek Talebi Kapatma İşlemi`)
+    .setDescription(`Destek talebini kapatmayı onaylamak için, \n10 saniye içinde \`evet\` yazınız.`)
+    .setFooter(`${client.user.username} | Destek Sistemi`)
+    message.channel.send(deneme)
+    .then((m) => {
+      message.channel.awaitMessages(response => response.content === 'evet', {
+        max: 1,
+        time: 10000,
+        errors: ['time'],
+      })
+      .then((collected) => {
+          message.channel.delete();
+        })
+        .catch(() => {
+          m.edit('Destek Talebi kapatma isteğin zaman aşımına uğradı!').then(m2 => {
+              m2.delete();
+          }, 3000);
+        });
+    });
+}
+});
+client.on("message", message => {
+    const dmchannel = client.channels.find("name", "dm-log");
+    if (message.channel.type === "dm") {
+        if (message.author.bot) return;
+        dmchannel.sendMessage("", {embed: {
+            color: 3447003,
+            title: `Gönderen: ${message.author.tag}`,
+            description: `Bota Özelden Gönderilen DM: ${message.content}`
+        }})
+    }
+});
+client.on('message', async msg => {
+  if (msg.content.toLowerCase() === 'sa') {
+    await msg.react('🇦');
+    msg.react('🇸');
+  }
+  });
+client.on('message', async message => {
+    if (message.content === 'se!debug') {
+        client.emit('guildMemberAdd', message.member || await message.guild.fetchMember(message.author));
+    }
+});
+client.on("guildMemberAdd", async member => {
+ let log = 'mod-log';
+let lbulundu = await db.fetch(`giriscikis_${member.guild.id}`);
+if (lbulundu === null) log = 'mod-log';
+else log = lbulundu;
+        
+  
+      
+    const channel = member.guild.channels.find('name', `${log}`);
+  
+  if (!channel) return;
+        let username = member.user.username;
+        let id = member.user.id;
+        if (channel === undefined || channel === null) return;
+        if (channel.type === "text") {            
+          const bg = await Jimp.read("https://cdn.discordapp.com/attachments/499911418896973824/500023171827761154/guildAdd_2.png");
+  const userimg = await Jimp.read(member.user.avatarURL);
+            var font;
+            if (member.user.tag.length < 15) font = await Jimp.loadFont(Jimp.FONT_SANS_64_WHITE);
+            else if (member.user.tag.length > 15) font = await Jimp.loadFont(Jimp.FONT_SANS_64_WHITE);
+            else font = await Jimp.loadFont(Jimp.FONT_SANS_32_WHITE);
+            await bg.print(font, 430, 170, member.user.tag);
+            await userimg.resize(362, 362);
+            await bg.composite(userimg, 43, 26).write("./img/"+ member.id + ".png");
+              setTimeout(function () {
+                    if( id == "KENDİ İD Nİ GİR" ){
+                     channel.send(new Discord.Attachment("./img/" + member.id + ".png"))
+                      channel.send("İşte Bak! Kurucum sunucuna giriş yaptı.")
+                    }else{
+                     channel.send(new Discord.Attachment("./img/" + member.id + ".png"));
+                    }
+              }, 1000);
+              setTimeout(function () {
+                fs.unlink("./img/" + member.id + ".png");
+              }, 10000);
+        }
+    })
+
+
+
+
+client.on("guildMemberRemove", async member => {
+  
+let log = 'mod-log';
+let lbulundu = await db.fetch(`giriscikis_${member.guild.id}`);
+if (lbulundu === null) log = 'mod-log';
+else log = lbulundu;
+           
+  
+    const channel = member.guild.channels.find('name', `${log}`);
+  if (!channel) return;
+        let username = member.user.username; 
+  let id = member.user.id
+        if (channel === undefined || channel === null) return;
+        if (channel.type === "text") {            
+          const bg = await Jimp.read("https://cdn.discordapp.com/attachments/499911418896973824/500023173459345416/guildRemove_2.png");
+ const userimg = await Jimp.read(member.user.avatarURL);
+            var font;
+            if (member.user.tag.length < 15) font = await Jimp.loadFont(Jimp.FONT_SANS_64_WHITE);
+            else if (member.user.tag.length > 15) font = await Jimp.loadFont(Jimp.FONT_SANS_64_WHITE);
+            else font = await Jimp.loadFont(Jimp.FONT_SANS_32_WHITE);
+            await bg.print(font, 430, 170, member.user.tag);
+            await userimg.resize(362, 362);
+            await bg.composite(userimg, 43, 26).write("./img/"+ member.id + ".png");
+              setTimeout(function () {
+                    if( id == "KENDİ İD Nİ GİR" ){
+                     channel.send(new Discord.Attachment("./img/" + member.id + ".png"))
+                      channel.send("Kurucum sunucunuzdan ayrıldı..")
+                    }else{
+                     channel.send(new Discord.Attachment("./img/" + member.id + ".png"));
+                    }
+              }, 1000);
+              setTimeout(function () {
+                fs.unlink("./img/" + member.id + ".png");
+              }, 10000);
+        }
+    })
+const yourID = ""; //Instructions on how to get this: https://redd.it/40zgse //Kendi İD'nizi Yazın
+const setupCMD = "" //İstediğiniz Komut Yapabilirsiniz örn : !kayıtol
+let initialMessage = ``; //Dilediğiniz Şeyi Yazabilirsiniz
+const roles = ["y", ""]; //İstediğiniz Rolü Yazabilirsiniz
+const reactions = ["", ""]; //İstediğiniz Emojiyi Ekleyebilirsiniz
+const botToken = "";  //Buraya botunuzun tokenini koyunuz
+                     
+//Load up the bot...
+const discord = require('discord.js');
+const bot = new Discord.Client();
+bot.login(botToken);
+//If there isn't a reaction for every role, scold the user!
+if (roles.length !== reactions.length) throw "Roles list and reactions list are not the same length!";
+//Function to generate the role messages, based on your settings
+function generateMessages(){
+    var messages = [];
+    messages.push(initialMessage);
+    for (let role of roles) messages.push(`Kayıt Olmak İçin **"${role}"** Emojisine Tıkla!`); //DONT CHANGE THIS
+    return messages;
+}
+bot.on("message", message => {
+    if (message.author.id == yourID && message.content.toLowerCase() == setupCMD){
+        var toSend = generateMessages();
+        let mappedArray = [[toSend[0], false], ...toSend.slice(1).map( (message, idx) => [message, reactions[idx]])];
+        for (let mapObj of mappedArray){
+            message.channel.send(mapObj[0]).then( sent => {
+                if (mapObj[1]){
+                  sent.react(mapObj[1]);  
+                } 
+            });
+        }
+    }
+})
+bot.on('raw', event => {
+    if (event.t === 'MESSAGE_REACTION_ADD' || event.t == "MESSAGE_REACTION_REMOVE"){
+        
+        let channel = bot.channels.get(event.d.channel_id);
+        let message = channel.fetchMessage(event.d.message_id).then(msg=> {
+        let user = msg.guild.members.get(event.d.user_id);
+        
+        if (msg.author.id == bot.user.id && msg.content != initialMessage){
+       
+            var re = `\\*\\*"(.+)?(?="\\*\\*)`;
+            var role = msg.content.match(re)[1];
+        
+            if (user.id != bot.user.id){
+                var roleObj = msg.guild.roles.find(r => r.name === role);
+                var memberObj = msg.guild.members.get(user.id);
+                
+                if (event.t === "MESSAGE_REACTION_ADD"){
+                    memberObj.addRole(roleObj)
+                } else {
+                    memberObj.removeRole(roleObj);
+                }
+            }
+        }
+        })
+ 
+    }   
+});
+
+
+
+client.login(ayarlar.token); "" //Token
